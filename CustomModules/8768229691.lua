e01e07a9a22564768acc3614238dc954df1f1276
ImNicknamez <136889440+ImNicknamez@users.noreply.github.com>
2023-07-06 11:09:39 +0200

    nokk

--- CustomModules/8768229691.lua
+++ CustomModules/8768229691.lua
@@ -1,16 +1,16 @@
-local function vapeGithubRequest(scripturl)
-	if not isfile("vape/"..scripturl) then
-		local suc, res = pcall(function() return game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/"..readfile("vape/commithash.txt").."/"..scripturl, true) end)
+local function xapeGithubRequest(scripturl)
+	if not isfile("xape/"..scripturl) then
+		local suc, res = pcall(function() return game:HttpGet("https://raw.githubusercontent.com/ImNicknamez/xaperell/"..readfile("xape/commithash.txt").."/"..scripturl, true) end)
 		if not suc or res == "404: Not Found" then return nil end
 		if scripturl:find(".lua") then res = "--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.\n"..res end
-		writefile("vape/"..scripturl, res)
+		writefile("xape/"..scripturl, res)
 	end
-	return readfile("vape/"..scripturl)
+	return readfile("xape/"..scripturl)
 end
 
 shared.CustomSaveVape = 8542275097
-if pcall(function() readfile("vape/CustomModules/8542275097.lua") end) then
-	loadstring(readfile("vape/CustomModules/8542275097.lua"))()
+if pcall(function() readfile("xape/CustomModules/8542275097.lua") end) then
+	loadstring(readfile("xape/CustomModules/8542275097.lua"))()
 else
 	local publicrepo = vapeGithubRequest("CustomModules/8542275097.lua")
 	if publicrepo then
